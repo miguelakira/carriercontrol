472d39f62735a98d61c02f576567d5424c373e50
Miguel Akira <miguel.akira@tagview.com.br>
2015-09-25 17:30:53 -0300

    Seeds pra popular o banco com carros e compradores

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.first_or_create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+puts "Generating 50 cars and buyers..."
+50.times do |count|
+
+  buyer = ""
+  if count % 2 == 0
+    buyer = Person.create(name: "nome #{count}", email: "email #{count}", rg: "rg #{count}", cpf: "cpf #{count}")
+  else
+    buyer = Company.create(name: "nome companhia #{count}", cnpj: "cnpj #{count}", email: "email #{count}")
+  end
+
+  Car.create(plate: "placa-#{count}", model: "modelo-#{count}", buyer: buyer)
+end
+
+puts "Done!"
