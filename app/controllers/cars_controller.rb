885bd83978916ba48285e69fc109317be1ff1d72
Miguel Akira <miguel.akira@gmail.com>
2016-01-06 11:59:50 -0200

    ajustando params de cars e buyer

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -10,14 +10,11 @@ class CarsController < ApplicationController
 
   def new
     @car = Car.new
-    @buyer = @car.buyer = Person.new
+    @buyer = @car.buyer = Company.new
   end
 
   def create
-    car_only_params = car_params
-    buyer_params = car_only_params.delete :buyer_attributes
-
-    @car = Car.new(car_only_params)
+    @car = Car.new(car_params)
     @buyer = @car.build_buyer(buyer_params)
 
     respond_to do |format|
@@ -33,7 +30,7 @@ class CarsController < ApplicationController
 
   def update
     respond_to do |format|
-      if @car.update(car_params)
+      if @car.update(car_and_buyer_params)
         format.html { redirect_to cars_path, notice: "Carro editado com sucesso" }
         format.json { render json: @car }
       else
@@ -55,7 +52,15 @@ class CarsController < ApplicationController
   end
 
   def car_params
-    params.require(:car).permit(:id, :plate, :model, :buyer_id, :delivery_status, :purchase_date, :expected_end_date, :buyer_type,
-      buyer_attributes: [:id, :name, :cpf, :cnpj])
+    params.require(:car).permit(:id, :plate, :model, :buyer_id, :delivery_status, :purchase_date, :expected_end_date, :buyer_type)
+  end
+
+  def buyer_params
+    buyer_params = params.require(:car).permit(buyer_attributes: [:id, :name, :cpf, :cnpj])
+    buyer_params[:buyer_attributes]
+  end
+
+  def car_and_buyer_params
+    car_params.merge(buyer_attributes: buyer_params)
   end
 end
