ffda7577260e20413af6459ea359c2be29a2e519
Miguel Akira <miguel.akira@gmail.com>
2016-01-20 18:28:43 -0200

    form e controller de finances

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -1,5 +1,5 @@
 class FinancesController < ApplicationController
-  before_action :set_finance, only: [:edit]
+  before_action :set_finance_and_client, only: [:edit]
 
   def index
     @people = Person.all
@@ -7,11 +7,16 @@ class FinancesController < ApplicationController
   end
 
   def edit
+    @payment = @finance.payments.build
+  end
 
+  def update
+    raise params.inspect
   end
 
   private
-  def set_finance
+  def set_finance_and_client
     @finance = Finance.find params[:id]
+    @client = @finance.client
   end
 end
--- app/models/finance.rb
+++ app/models/finance.rb
@@ -2,6 +2,8 @@ class Finance < ActiveRecord::Base
   has_many :payments
   belongs_to :client, polymorphic: true
 
+  accepts_nested_attributes_for :payments
+
   def total_debt
     client.freights.map(&:total).sum
   end
